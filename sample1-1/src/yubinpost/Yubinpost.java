03a9850c57be8d4b94d877e803742196a429166e
右田　大輔 <dune116911@yahoo.co.jp>
2013-12-19 01:24:29 +0900

    例題1郵便ポスト、例題2図形描画プログラム追加

--- sample1-1/src/yubinpost/Yubinpost.java
+++ sample1-1/src/yubinpost/Yubinpost.java
@@ -1,5 +1,16 @@
 package yubinpost;
 
-public class Yubinpost {
+import java.applet.Applet;
+import java.awt.Graphics;
 
+public class Yubinpost extends Applet 
+{
+public void paint(Graphics g)
+{
+	
+	g.drawRect(90, 15, 70, 90);
+	g.drawRect(100, 35, 50, 15);
+	g.drawRect(115, 105, 20, 30);
+	
+}
 }
--- sample1-1/src/zukeibyouga/Zukeibyouga.java
+++ sample1-1/src/zukeibyouga/Zukeibyouga.java
@@ -1,5 +1,20 @@
 package zukeibyouga;
 
-public class Zukeibyouga {
+import java.applet.Applet;
+import java.awt.Graphics;
 
+public class  Zukeibyouga extends Applet
+{
+public void paint(Graphics g){
+	
+	g.drawRect(30,30,40,100);
+	g.fillRect(40,40,20,80);
+	g.drawLine(160, 80, 220, 120);
+	g.drawOval(90,30,60,40);
+	g.fillOval(100, 40, 40, 20);
+	g.drawRoundRect(90, 80, 60, 60, 40, 40);
+	g.fillRoundRect(100, 90, 40, 40, 20, 20);
+	g.drawArc(120, 60, 120, 80, 240, 150);
+	g.fillArc(120, 30, 120, 80, 0, 90);
+}
 }
