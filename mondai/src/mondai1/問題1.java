dbf5d390f8596fea51683b298bb4abb0ce2fcf52
右田　大輔 <dune116911@yahoo.co.jp>
2013-12-27 00:12:33 +0900

    Javaテキスト問題1
    Yubinpostからデータいじっただけ。
    これが課題・・・？

---
+++ mondai/src/mondai1/問題1.java
@@ -0,0 +1,17 @@
+package mondai1;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+
+public class 問題1 extends Applet{
+	public void paint(Graphics g){
+		
+		g.drawRect(90, 15, 70, 90);
+		g.drawRect(100, 35, 50, 15);
+		g.drawRect(105,105,40,30); //Yubinpostからデータ変更しただけ
+		
+		
+	}
+	
+}
