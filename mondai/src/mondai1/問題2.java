37c1f71af0680fcfb04b97173115e500997a3346
右田　大輔 <dune116911@yahoo.co.jp>
2013-12-27 00:49:11 +0900

    Javaテキスト問題2
    zukeibyougaのデータ変更版

---
+++ mondai/src/mondai1/問題2.java
@@ -0,0 +1,15 @@
+package mondai1;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class 問題2 extends Applet{
+	public void paint(Graphics g){
+		
+		g.drawRect(60, 20, 160, 100);
+		g.drawRect(40, 60, 100, 80);
+		g.drawRect(100,40,140,60);
+		g.drawRect(180, 80, 20, 60);
+	}
+
+}
